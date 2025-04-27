710a46ee8764421526c3392e7dcfad8b821d5c1d
TariqNew <marandumashaka78@gmail.com>
2025-04-27 19:23:47 +0300

    Route: Modified for the data with Id

--- Route/Route.js
+++ Route/Route.js
@@ -1,21 +1,26 @@
 import { fetchDataAll, fetchDataId, fetchDataContact, fetchDataAbout } from "../Controllers/Controller.js";
 
 export const router = (req, res) => {
-    if (req.url === "/" && req.method === "GET") {
-        fetchDataAll(req, res); 
+    const url = req.url.split('?')[0]; 
+    const method = req.method;
+
+    // Handle dynamic user ID: /user/:id
+    if (url.startsWith("/user/") && method === "GET") {
+        const id = url.split("/")[2]; 
+        fetchDataId(req, res, id); 
     }
-    else if (req.url === "/user" && req.method === "GET") {
-        fetchDataId(req, res); 
+    else if (url === "/" && method === "GET") {
+        fetchDataAll(req, res);
     }
-    else if (req.url === "/contact" && req.method === "GET") {
-        fetchDataContact(req, res); 
+    else if (url === "/contact" && method === "GET") {
+        fetchDataContact(req, res);
     }
-    else if (req.url === "/about" && req.method === "GET") {
-        fetchDataAbout(req, res); 
+    else if (url === "/about" && method === "GET") {
+        fetchDataAbout(req, res);
     }
     else {
         res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end("<h1>404 - Page Not Found</h1>");
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ message: "404 - Page Not Found" }));
     }
 }
